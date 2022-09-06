a8eca401e505f50fea89139f9ecc2b4e6571c0d9
Shamil Sharafeev <ssharafeev@nic.ru>
2022-09-06 17:39:13 +0300

    Accrual system

--- internal/app/services/order.go
+++ internal/app/services/order.go
@@ -7,6 +7,7 @@ import (
 	"github.com/phrasetagg/gofermart/internal/app/repositories"
 	"regexp"
 	"sort"
+	"strconv"
 )
 
 type Order struct {
@@ -62,10 +63,38 @@ func (o *Order) Upload(userID int64, orderNumber string) error {
 func IsValidOrderNumber(orderNumber string) bool {
 	re := regexp.MustCompile("^([0-9])+$")
 
-	return re.MatchString(orderNumber)
+	intNumber, err := strconv.Atoi(orderNumber)
+	if err != nil {
+		return false
+	}
+
+	return re.MatchString(orderNumber) && ValidateLunaAlgorithm(intNumber)
 }
 
 // IsNotValidOrderNumber инвертированный метод IsValidOrderNumber.
 func IsNotValidOrderNumber(orderNumber string) bool {
 	return !IsValidOrderNumber(orderNumber)
+}
+
+func ValidateLunaAlgorithm(number int) bool {
+	return (number%10+lunaChecksum(number/10))%10 == 0
+}
+
+func lunaChecksum(number int) int {
+	var luhn int
+
+	for i := 0; number > 0; i++ {
+		cur := number % 10
+
+		if i%2 == 0 { // even
+			cur = cur * 2
+			if cur > 9 {
+				cur = cur%10 + cur/10
+			}
+		}
+
+		luhn += cur
+		number = number / 10
+	}
+	return luhn % 10
 }
